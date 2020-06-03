78f5be332ac9b79a75d199e332e0ca2aee194cc8
Poyraz-Ozmen <34272299+Poyraz-Ozmen@users.noreply.github.com>
2020-06-04 00:03:21 +0300

    Update 3.cpp
    Recursive method.

--- 3.cpp
+++ 3.cpp
@@ -1,76 +1,81 @@
-int char_to_ascii(char c) {
-   int i = c;
-   return i;
-   //cout<<"The ASCII value of "<<c<<" is "<<i<<endl;
-}
+// #include<bits/stdc++.h> 
+//function to convert string into lowercase
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-    queue <char> q; 
-    //string s = "bcdkak112345azxvnmpoiu98765ga";
-    string subStr = "";
-    vector<string> subStr_list;
-    transform(s.begin(), s.end(), s.begin(), ::tolower); 
-    vector<char> charTable;
-    
-    for(int i=0; i<300; i++){
-        charTable.push_back('A'); // since  string has converted to lowercase letters, we can fill up initial char table with char 'A'
+   
+    struct cell{
+        char c;
+        int index;
+    };
+   
+    int char_to_index(char c){
+        // 'a' is 97, whole string will be converted into lowercase str.
+        return int(c) - 97;
     }
     
     
-    for (int i = 0; i < s.size(); i++)
-    {
-        if(charTable[char_to_ascii(s[i])]==s[i])
-        { // duplicate char
-            q.push(s[i]);
-            int counter=0;
-            int sizeCounter =0;
-            int queueSize = q.size();
-            queue <char> temp; 
-
-            while (counter != 2 and sizeCounter<queueSize) 
-            {   
-                if(q.front()==s[i]) {counter++;}
-
-                if(counter>=1 )  
-                {   
-                    if(counter!=2){subStr.push_back(q.front());}
-                    
-                    if(q.front()!=s[i] or counter==2){
-                        temp.push(q.front());
-                    }
-                    q.pop();
+    string toLower(string str){
+        transform(str.begin(), str.end(), str.begin(), ::tolower);
+        return str;
+    }
+    
+    
+    vector<string> RecursiveSub ( string s, vector<cell> & hashTable, vector<string> & string_vec)
+    {   
+        
+        for(int i = 0; i  <  s.size(); i++)
+        {
+            int index = char_to_index(s[i]);
+            string temp = "";
+            if(hashTable[index]==NULL)
+            {
+                temp = temp + s[i];
+                hashTable[index] = s[i];
+            }
+            
+            else{
+                
+                // there is a collision
+                //  assume string is "123454........N"
+                // push_back 12345 to string_vector
+                // go on with 54........N,   (cut the string)
+                
+                
+                string sub_str = s.substr(i, s.size()- i);
+                
+                string_vec.push_back(  s.substr(0,i-1)  );  //  123454........N
+                
+                // now clear 12345 from hashTable    
+                for(int k = 0; k<i;  k++){
+                    index = char_to_index(s[k]);
+                    hashTable[index] = NULL;
                 }
                 
-                //if(q.front()==s[i] and counter==1) {q.pop();}
                 
-                if(counter<1){
-                    subStr.push_back(q.front());
-                    charTable[char_to_ascii(q.front())] = 'A';
-                    q.pop();
-
-                    } // assume string is "12345abcdef5" then 12345 should be removed for the next round. counter increases when it sees duplicate char which is '5'
-                sizeCounter++;
+                return RecursiveSub(sub_str,hashTable,string_vec);
             }
-            //counter = 0;
-            subStr_list.push_back(subStr);
-            subStr="";           
-            q = temp;
         }
-        else{
-            q.push(s[i]);
-            charTable[char_to_ascii(s[i])] = s[i];
-        }
-    }
-
-    //subStr_list.push_back(subStr);
-    int max = subStr_list[0].size();
-    for(int j=0; j<subStr_list.size();j++){
-        if(subStr_list[j].size()>max){max = subStr_list[j].size();}
+        
+        // if there left a string without collision we should add it to string_vec
+        if(temp.size>0) {   string_vec.push_back(temp);     }
+        
+        return string_vec;
+        
     }
     
-    return max;
-}     
+   
+    int lengthOfLongestSubstring(string s)
+    {
+        vector<string> string_vec;
+        vector<char> hashTable(30);
+        
+        s = toLower(s); // converted to lowercase string
+      
+        string_vec =  RecursiveSub(s,hashTable,string_vec);
     
+        
+        
+        return 0;
+    }
 };
